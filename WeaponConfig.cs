50a1599aa67f15c5a15ff94b15e49602abf1a6a1
404JimTV <broadjim1996@gmail.com>
2026-05-09 10:06:01 +0100

    Add files via upload

---
+++ WeaponConfig.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace
+{
+    public class  : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.SetNameOverride("");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = ;
+            Item.DamageType = ;
+            Item.width = ;
+            Item.height = ;
+            Item.useTime = ;
+            Item.useAnimation = ;
+            Item.useStyle = ;
+            Item.noMelee = ;
+            Item.knockBack = ;
+            Item.value = Item.buyPrice();
+            Item.rare = ItemRarityID.;
+            Item.UseSound = SoundID.Item;
+            Item.autoReuse = ;
+            Item.shoot = ProjectileID.Bullet;
+            Item.shootSpeed = ;
+            Item.useAmmo = AmmoID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient();
+            recipe.AddIngredient();
+            recipe.AddTile();
+            recipe.Register();
+        }
+    }
+}
